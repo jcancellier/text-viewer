7b6fae8970d47521a1c1cf2046f48f811ce197dc
jcancellier <sonicjcan@hotmail.com>
2023-12-04 02:02:09 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [formattedText, setFormattedText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleFormatClick = () => {
+    const formatted = inputText.replace(/&lrm;|<[^>]*>/g, '');
+    setFormattedText(formatted);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Input Text:
+        <textarea value={inputText} onChange={handleInputChange} />
+      </label>
+      <br />
+      <button onClick={handleFormatClick}>Format Text</button>
+      <br />
+      <h2>Formatted Text</h2>
+      <div style={{ whiteSpace: 'pre-line' }}>{formattedText}</div></header>
     </div>
   );
 }
