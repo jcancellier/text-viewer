f08c3cd936a6e728b7ca517bc87e0ae45ee52306
jcancellier <sonicjcan@hotmail.com>
2023-12-04 14:35:05 -0800

    save scroll position and formatted text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const FORMATTED_TEXT_KEY = 'formattedText';
+const SCROLL_POSITION_KEY = 'scrollPosition';
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [formattedText, setFormattedText] = useState('');
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const storedFormattedText = localStorage.getItem(FORMATTED_TEXT_KEY);
+    const storedScrollPosition = localStorage.getItem(SCROLL_POSITION_KEY);
+
+    if (storedFormattedText) {
+      setFormattedText(storedFormattedText);
+    }
+
+    if (storedScrollPosition) {
+      const parsedScrollPosition = parseInt(storedScrollPosition, 10)
+      setScrollPosition(parsedScrollPosition);
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  // On component mount, if scroll position was set then scroll to position.
+  useEffect(() => {
+    window.scrollTo(0, scrollPosition);
+  }, [scrollPosition])
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
@@ -12,6 +41,11 @@ function App() {
   const handleFormatClick = () => {
     const formatted = inputText.replace(/&lrm;|<[^>]*>/g, '');
     setFormattedText(formatted);
+    localStorage.setItem(FORMATTED_TEXT_KEY, formatted);
+  };
+
+  const handleBeforeUnload = () => {
+    localStorage.setItem(SCROLL_POSITION_KEY, window.scrollY);
   };
 
   return (
